b5f41f039c3a4ae1fd98c76c3679a9c2c1f10e78
Jing <1392039476@qq.com>
2023-06-06 22:15:03 +0800

    dev

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,14 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/v_model',
+    name: 'v_model',
+    component: () => import('../views/v_model.vue')
+  },
+
 ]
 
 const router = new VueRouter({
